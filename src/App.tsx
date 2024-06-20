ef9a5ab31fd110b50e9b4c953761179f96e2bb0c
Kyle Lerio <kylelerio15@gmail.com>
2024-06-20 18:34:32 +0800

    Pass the children

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import "./App.css";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <>
-      <ListGroup
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </>
   );
 }
