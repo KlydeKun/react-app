ea9cc44df136205291457e95818f52b0a4f86c06
Kyle Lerio <kylelerio15@gmail.com>
2024-06-20 18:04:21 +0800

    Finish creating a listGroup component, using fragment, rendering list, conditional rendering, handling events, managing state, and passing data via props(properties)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  return (
+    <>
+      <ListGroup items={items} heading="Cities" />
+    </>
+  );
+}
+
+export default App;
