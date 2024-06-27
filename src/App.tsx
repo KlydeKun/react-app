b6359b95b1d04556222705949231054fb861bf27
Kyle Lerio <kylelerio15@gmail.com>
2024-06-27 18:33:06 +0800

    Build a form component

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import ListGroup from "./components/ListGroup";
 import NestedObject from "./components/NestedObject";
 import SharedState from "./components/SharedState";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
 function App() {
   // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -15,12 +17,18 @@ function App() {
   // const handleSelectItem = (item: string) => {
   //   console.log(item);
   // };
+  const [selectedCategory, setSelectedCategory] = useState("");
+
   const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
     { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Entertainment" },
     { id: 4, description: "ddd", amount: 10, category: "Utilities" },
   ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <>
@@ -43,8 +51,20 @@ function App() {
       </div> */}
       {/* <SharedState /> */}
       {/* <Form /> */}
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
       <ExpenseList
-        expenses={expenses}
+        expenses={visibleExpenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </>
