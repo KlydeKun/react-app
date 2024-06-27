32261b28b4e3b12dbde6cf8bc51286db414ee877
Kyle Lerio <kylelerio15@gmail.com>
2024-06-27 16:44:53 +0800

    Build a expense list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Alert from "./components/Alert";
 import BugUpdate from "./components/BugUpdate";
 import Button from "./components/Button";
@@ -6,6 +7,7 @@ import Form from "./components/Form";
 import ListGroup from "./components/ListGroup";
 import NestedObject from "./components/NestedObject";
 import SharedState from "./components/SharedState";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
   // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
@@ -13,6 +15,12 @@ function App() {
   // const handleSelectItem = (item: string) => {
   //   console.log(item);
   // };
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
 
   return (
     <>
@@ -34,7 +42,11 @@ function App() {
         <NestedObject />
       </div> */}
       {/* <SharedState /> */}
-      <Form />
+      {/* <Form /> */}
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 }
